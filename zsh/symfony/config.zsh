6777ced45ba8f393e6211631405df17910858905
Daniel Rotter <daniel.rotter@gmail.com>
2015-08-26 16:09:24 +0200

    fixed symfony app/console autocomplete

--- zsh/symfony/config.zsh
+++ zsh/symfony/config.zsh
@@ -1,7 +1,7 @@
 # Adapted from code found at <https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/symfony2/symfony2.plugin.zsh>
 
 _symfony2_get_command_list () {
-	php app/console --no-ansi | sed "1,/Available commands/d" | awk '/^  [a-z]+/ { print $1 }'
+	php app/console --no-ansi | sed "1,/Available commands/d" | awk '/^  ?[a-z]+/ { print $1 }'
 }
 
 _symfony2 () {
